cbfc7ee685982bbaba6eb78c7e1b27f5a06da9dc
Hack Reactor Student <student@hackreactor.com>
2017-04-14 17:06:42 -0700

    First commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
-angular.module('video-player')
+angular.module('video-player', [])
+	
+.controller('ctrl', function($scope) {
+$scope.link = "/src/data/exampleVideoData.js"
+})
 
 .directive('app', function() {
   return {
     // TODO
+    templateUrl: "src/templates/app.html"
   };
 });
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -2,5 +2,17 @@ angular.module('video-player')
 .directive('videoList', function() {
   return {
     // TODO
+    scope: {
+    videos: '<',//class="todos"
+    onClick: '<'
+  },
+
+    templateUrl: "src/templates/videoList.html",
+    restrict: 'E', 
+    controllerAs: 'ctrl', 
+    bindToController: true, 
+    controller: function() {
+    //   console.log(videos[0]);
+    }
   };
 });
